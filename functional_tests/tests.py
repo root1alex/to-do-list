2135c1d223c3f4a8c98a751b3ba24dd48b9c36e0
admin <dd@dd.com>
2016-03-15 10:37:14 +0400

    add check unique url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,6 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
@@ -65,3 +67,35 @@ class NewVisitorTest(LiveServerTestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+
+        ###
+        # Now a new user, Francis, comes along to the site.
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from coockies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertionNotIn('Buy peacock feathers', page_text)
+        self.assertionNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entrering a new item. he
+        # is liess interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Franceis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
