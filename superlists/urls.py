93e33cb9ece76264894a8b4e815689778fd98b41
admin <dd@dd.com>
2016-03-16 11:50:55 +0400

    add new URL + view for adding to existing list.FT passes

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     url(r'^lists/the-only-list-in-the-world/$', views.view_list,
         name='view-list'),
     url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
