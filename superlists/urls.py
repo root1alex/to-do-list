d43d76fd5cb821f9991ed81c07ebac41c5e3621d
admin <dd@dd.com>
2016-03-16 12:01:39 +0400

    url refactor, all tests are passed

--- superlists/urls.py
+++ superlists/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from lists import views
+from lists import urls as list_urls
 
 urlpatterns = [
     # Examples:
@@ -10,9 +11,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list,
-        name='view-list'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^lists/', include(list_urls)),
 ]
