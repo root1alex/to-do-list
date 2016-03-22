310d399a766ac4d34851d06d82583891f76951ee
admin <dd@dd.com>
2016-03-22 15:18:28 +0400

    fixing fabfile DEBUG option

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -135,3 +135,19 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 512,
                 delta=5
         )
+    
+    def test_cannot_add_empty_list_items(self):
+        # Edith gos to the home page and accidentally tries to submit
+        # an empty item. She hist Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # than list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversel, she now dices to submit a second blank list item
+
+        # She recives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
