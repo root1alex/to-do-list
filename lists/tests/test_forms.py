161c3f5e14862f45ae56e8fcb746390ff951ec63
admin <dd@dd.com>
2016-03-24 10:25:43 +0400

    use new form in home_page, simplify tests.NB breaks stuff

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -5,10 +5,6 @@ from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class ItemFormTest(TestCase):
 
-#    def test_form_renders_item_text_input(self):
-#        form = ItemForm()
-#        self.fail(form.as_p())
-    
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
