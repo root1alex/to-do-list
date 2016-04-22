544cbb9b04f714547d02d145cdbef7063f324704
admin <dd@dd.com>
2016-04-22 15:18:18 +0400

    REPO_URL in fabfile, requirements.txt

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,10 +2,10 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL = 'https://github.com/hjwp/book-examle.git'
+REPO_URL = 'https://github.com/root1alex/to-do-list.git'
 
 def deploy():
-    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+    site_folder = '/home/%s/sites/%s' % (env.user, env.host) 
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
@@ -22,7 +22,7 @@ def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
         run('cd %s && git fetch' % (source_folder,))
     else:
-        run('git clone %s %s' % ('https://github.com/root1alex/to-do-list.git', source_folder))
+        run('git clone %s %s' % (REPO_URL, source_folder))
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
